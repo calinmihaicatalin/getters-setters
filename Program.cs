5dab5981674a45cf2f71db582f808d885b14c685
Mihai <earlkingmajesty@gmail.com>
2024-11-05 02:47:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+Car car = new Car(0);
+car.Speed = 100000000;
+Car car1 = new Car("Porsche");
+
+Console.WriteLine(car1.Model);
+Console.WriteLine(car.Speed);
+
+Console.ReadKey();
+
+class Car
+{
+    private int speed;
+    public String Model { get; set; }
+    public Car(String model)
+    {
+        this.Model = model;
+    }
+
+    public Car (int speed)
+    {
+        Speed = speed;
+    }
+    public int Speed
+    {
+        get { return speed; }//read
+        set //writable
+        {
+            if (value > 500)
+            {
+                speed = 500;
+            }
+            else
+            {
+                speed = value;
+            }
+        }
+    }
+}
